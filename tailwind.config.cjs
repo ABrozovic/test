bd05399f67d394a25682dff8a105950ec1db7ebd
Brozovic <alejandro.brozovic@gmail.com>
2022-08-16 22:03:57 -0400

    ci: tailwind theme and dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        darkpurple: "#260C1A",
+        lavender: "#826982",
+        test: "#EB9B96",
+      },
+    },
   },
+  darkMode: 'class',
   plugins: [],
 };
